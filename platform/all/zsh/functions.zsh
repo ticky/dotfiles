0be4787a6b89a880427f73d83975ea8c856d6e59
Geoff Stokes <geoff@geoffstokes.net>
2013-09-12 17:51:57 +1000

    Clear screen when running Play

--- platform/all/zsh/functions.zsh
+++ platform/all/zsh/functions.zsh
@@ -110,6 +110,7 @@ fi
 # suppress "Boot" app in dock when running an app in the Play framework
 if /usr/libexec/java_home >/dev/null 2>&1 && play --version >/dev/null 2>&1; then
   function play {
+    clear 2>/dev/null
     _JAVA_OPTIONS="-Djava.awt.headless=true" command play $@
     # restore echo after play 2.1.3+ murders it (Java is great)
     stty echo
