f9e6d472d411154d62446053841a4849b14af1d2
Geoff Stokes <geoff@geoffstokes.net>
2013-09-12 15:47:11 +1000

    Update shell options

--- platform/all/zsh/shell.zsh
+++ platform/all/zsh/shell.zsh
@@ -1,10 +1,13 @@
+setopt AUTO_CD
+setopt AUTO_PUSHD
+setopt CHASE_DOTS
+setopt COMPLETE_ALIASES
+setopt COMPLETE_IN_WORD
+setopt CORRECT
+setopt IGNORE_EOF
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
 setopt PROMPT_SUBST
-setopt CORRECT
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-setopt complete_aliases
