22916596afb68d22534104f7f564328de7ecf25f
Geoff Stokes <geoff@geoffstokes.net>
2014-02-11 17:46:53 +1100

    Move finder path retrieval to its own script

--- platform/darwin/zsh/functions.zsh
+++ platform/darwin/zsh/functions.zsh
@@ -1,4 +1,4 @@
 # Change working directory to the top-most Finder window location
 function cdf() { # short for `cdfinder`
-  cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')"
+  cd "$(finder-path)"
 }
