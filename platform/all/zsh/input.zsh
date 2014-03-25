13d532bcd1663de07b59cce5f5768277d3f31072
Geoff Stokes <geoff@geoffstokes.net>
2014-03-25 15:25:05 +1100

    Fixes for some key bindings inside tmux
    - also improve compatibility with non-OS X hosts

--- platform/all/zsh/input.zsh
+++ platform/all/zsh/input.zsh
@@ -1,16 +1,24 @@
 bindkey -e
-bindkey '^[[1;9D' backward-word
-bindkey '^[[1;9C' forward-word
 
 # for rxvt
 bindkey "\e[8~" end-of-line
 bindkey "\e[7~" beginning-of-line
+
 # for non RH/Debian xterm, can't hurt for RH/Debian xterm
 bindkey "\eOH" beginning-of-line
 bindkey "\eOF" end-of-line
+
 # for freebsd / OS X console
+bindkey '^[[1;9D' backward-word
+bindkey '^[[1;9C' forward-word
 bindkey "\e[H" beginning-of-line
 bindkey "\e[F" end-of-line
+
+# for OS X tmux
+bindkey '^[[1;3D' backward-word
+bindkey '^[[1;3C' forward-word
+bindkey "\e[1~" beginning-of-line
+bindkey "\e[4~" end-of-line
 
 # Up/down arrow.
 # I want shared history for ^R, but I don't want another shell's activity to
