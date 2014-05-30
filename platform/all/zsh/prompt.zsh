a0999489ec34cc44fa81b23103ce3aa007989bf5
Geoff Stokes <geoff@geoffstokes.net>
2014-05-30 16:49:09 +1000

    Add support for ZSH syntax highlighting

--- platform/all/zsh/prompt.zsh
+++ platform/all/zsh/prompt.zsh
@@ -77,6 +77,61 @@ export PROMPT=$'%{$bg_bold[$PROMPT_PRIMARY]%}%n@%m%{$reset_color%}%{$fg[$PROMPT_
 export RPROMPT=$'$(git_status) $(zdate s)'
 export PS2=$'%{$fg[$PROMPT_PRIMARY]%}› %{$reset_color%}'
 
+if [[ -r /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]]; then
+
+  source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets)
+
+  # Default Formatting
+  ZSH_HIGHLIGHT_STYLES[default]="none"
+
+  # Commands
+  ZSH_HIGHLIGHT_STYLES[alias]="none"
+  ZSH_HIGHLIGHT_STYLES[builtin]="none"
+  ZSH_HIGHLIGHT_STYLES[function]="none"
+  ZSH_HIGHLIGHT_STYLES[command]="none"
+  ZSH_HIGHLIGHT_STYLES[hashed-command]="none"
+
+  # Command Prefix (sudo, buitin, etc.)
+  ZSH_HIGHLIGHT_STYLES[precommand]="fg=yellow,bold"
+
+  # Missing
+  ZSH_HIGHLIGHT_STYLES[unknown-token]="fg=red,bold"
+  ZSH_HIGHLIGHT_STYLES[reserved-word]="fg=red"
+
+  # Command Separators (;, ||, &&, etc.)
+  ZSH_HIGHLIGHT_STYLES[commandseparator]="fg=green"
+
+  # Globbing and Paths
+  ZSH_HIGHLIGHT_STYLES[path]="underline"
+  ZSH_HIGHLIGHT_STYLES[path_prefix]="underline"
+  ZSH_HIGHLIGHT_STYLES[path_approx]="fg=yellow,underline"
+  ZSH_HIGHLIGHT_STYLES[globbing]="fg=blue,bold"
+
+  # History Expansion Characters (!, ^, #)
+  ZSH_HIGHLIGHT_STYLES[history-expansion]="fg=blue"
+
+  # Options
+  ZSH_HIGHLIGHT_STYLES[single-hyphen-option]="fg=blue"
+  ZSH_HIGHLIGHT_STYLES[double-hyphen-option]="fg=blue"
+
+  # Strings and quoted arguments
+  ZSH_HIGHLIGHT_STYLES[back-quoted-argument]="none"
+  ZSH_HIGHLIGHT_STYLES[single-quoted-argument]="fg=green"
+  ZSH_HIGHLIGHT_STYLES[double-quoted-argument]="fg=green"
+  ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]="fg=cyan"
+  ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]="fg=cyan"
+
+  # Variable Assignments
+  ZSH_HIGHLIGHT_STYLES[assign]="none"
+
+  # Mismatched brackets
+  ZSH_HIGHLIGHT_STYLES[bracket-error]="fg=red,standout"
+  ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]="standout"
+
+fi
+
 precmd() {
   title "zsh" "%~ — %n@%m" "%55<...<%~"
 }
