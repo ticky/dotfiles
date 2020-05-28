6057822fbf40e488cc9ce9872941060b9b8f3a1b
Jessica Stokes <hello@jessicastokes.net>
2020-05-28 10:41:35 -0700

    Stop Homebrew from auto-updating and auto-cleaning

--- platform/darwin/zsh/exports.zsh
+++ platform/darwin/zsh/exports.zsh
@@ -1,2 +1,4 @@
 export HOMEBREW_INSTALL_BADGE=✨         # Sparkles!!!
 export HOMEBREW_IS_FOR_WITCHES=OBVIOUSLY # Future proofing
+export HOMEBREW_NO_AUTO_UPDATE=TRUE      # Please stop breaking things
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE  # I appreciate the gesture, but maybe ask first
