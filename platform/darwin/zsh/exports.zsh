62f7d20375b066eb4e0daf0ab5be10e52e896aa5
Jessica Stokes <hello@jessicastokes.net>
2020-12-21 12:17:13 -0800

    Configure `make` to use 1.5 * the CPU count by default

--- platform/darwin/zsh/exports.zsh
+++ platform/darwin/zsh/exports.zsh
@@ -2,3 +2,5 @@ export HOMEBREW_INSTALL_BADGE=✨         # Sparkles!!!
 export HOMEBREW_IS_FOR_WITCHES=OBVIOUSLY # Future proofing
 export HOMEBREW_NO_AUTO_UPDATE=TRUE      # Please stop breaking things
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE  # I appreciate the gesture, but maybe ask first
+
+export MAKEFLAGS="-j $(printf "%.0f" $(( $(sysctl -n hw.ncpu) * 1.5 )))" # Configure `make` to use all available cores as best it can
